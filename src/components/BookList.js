abc3bfc3561a75fab1a8b1c2e8105cb0447ebf93
elifcetinkaya <eliifctnk@gmail.com>
2021-05-31 16:46:49 +0300

    context api usage II, provider-consumer

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,33 +1,33 @@
 import React from "react";
 import Book from "./Book";
 import "./BookList.css";
-import BookContext from "../context/BookContext";
+import { BookContext } from "../context/BookContext";
 
 class BookList extends React.Component {
-  static contextType = BookContext;
-
   render() {
     console.log(this.context);
 
-    const books = this.context;
-
-    return (
-      <section className="page-section bg-light" id="portfolio">
-        <div className="container">
-          <div className="text-center">
-            <h2 className="section-heading text-uppercase">BookFolio</h2>
-            <h3 className="section-subheading text-muted">
-              Lorem ipsum dolor sit amet consectetur.
-            </h3>
+    return <BookContext.Consumer>
+      {value => {
+        return (
+          <section className="page-section bg-light" id="portfolio">
+          <div className="container">
+            <div className="text-center">
+              <h2 className="section-heading text-uppercase">BookFolio</h2>
+              <h3 className="section-subheading text-muted">
+                Lorem ipsum dolor sit amet consectetur.
+              </h3>
+            </div>
+            <div className="row">
+              {value.books.map((book, i) => {
+                return <Book book={book} key={i} />;
+              })}
+            </div>
           </div>
-          <div className="row">
-            {books.map((book, i) => {
-              return <Book book={book} key={i} />;
-            })}
-          </div>
-        </div>
-      </section>
-    );
+        </section>
+        )
+      }}
+    </BookContext.Consumer>;
   }
 }
 
--- src/context/BookContext.js
+++ src/context/BookContext.js
@@ -1,75 +1,93 @@
-import React from 'react';
+import React from "react";
 
 //Context oluşturma.
-export default React.createContext(
-    [
-        {
-          title: "Kırmızı Pazartesi",
-          author: "Gabriel Garcia Marquez",
-          pageNum: 296,
-          imageURL:
-            "https://i.idefix.com/cache/600x600-0/originals/0000000064101-1.jpg",
-          topic:
-            "1968-1970 yılları arasında geçen olaylar, o günün toplumsal gerçeklerini de satırlara taşıyor. Ama romanın odağında bu toplumsal olaylar değil üçlü bir aşk var. Gençliğin rüzgârıyla hareketlenen İmkânsızın Şarkısını ölümle erken karşılaşan gençlerin hayatı yönlendiriyor. Hiçbir şeyin önem taşımadığı, amaçsızlığın ağır bastığı, özgür seksin kol gezdiği bir öğrenci hayatı... Ama diğer yanda da yoğun duygular var... İmkânsız aşklar, imkânsız şarkılar söyleten. Hemen hemen her Japon gencinin okuduğu roman anayurdu dışında da çok kişi tarafından sahipleniliyor.",
-        },
-        {
-          title: "Şeker Portakalı",
-          author: "Jose Muro de Vasconselos",
-          pageNum: 200,
-          imageURL:
-            "https://i.idefix.com/cache/600x600-0/originals/0000000064031-1.jpg",
-          topic:
-            "İrlandalı yazar Bram Stoker’ın, iki taraf arasındaki bu irade ve güç çatışmasını işlediği ve korku edebiyatının başyapıtlarından biri sayılan Dracula, yayımlanmasının üzerinden yüz yılı aşkın süre geçmesine karşın, bugün de aynı ilgiyle okunuyor.",
-        },
-        {
-          title: "En Uzun Yüzyıl",
-          author: "İlber Ortaylı",
-          pageNum: 296,
-          imageURL:
-            "https://i.idefix.com/cache/600x600-0/originals/0001744876001-1.jpg",
-          topic:
-            "1968-1970 yılları arasında geçen olaylar, o günün toplumsal gerçeklerini de satırlara taşıyor. Ama romanın odağında bu toplumsal olaylar değil üçlü bir aşk var. Gençliğin rüzgârıyla hareketlenen İmkânsızın Şarkısını ölümle erken karşılaşan gençlerin hayatı yönlendiriyor. Hiçbir şeyin önem taşımadığı, amaçsızlığın ağır bastığı, özgür seksin kol gezdiği bir öğrenci hayatı... Ama diğer yanda da yoğun duygular var... İmkânsız aşklar, imkânsız şarkılar söyleten. Hemen hemen her Japon gencinin okuduğu roman anayurdu dışında da çok kişi tarafından sahipleniliyor.",
-        },
-        {
-          title: "Dracula",
-          author: "Bram Stoker",
-          pageNum: 200,
-          imageURL:
-            "https://i.idefix.com/cache/600x600-0/originals/0001828853001-1.jpg",
-          topic:
-            "İrlandalı yazar Bram Stoker’ın, iki taraf arasındaki bu irade ve güç çatışmasını işlediği ve korku edebiyatının başyapıtlarından biri sayılan Dracula, yayımlanmasının üzerinden yüz yılı aşkın süre geçmesine karşın, bugün de aynı ilgiyle okunuyor.",
-        },
-        {
-          title: "Karamazov Kardeşler",
-          author: "Fyodor Mihayloviç Dostoyevski",
-          pageNum: 200,
-          imageURL:
-            "https://i.idefix.com/cache/600x600-0/originals/0001803800001-1.jpg",
-          topic:
-            "İrlandalı yazar Bram Stoker’ın, iki taraf arasındaki bu irade ve güç çatışmasını işlediği ve korku edebiyatının başyapıtlarından biri sayılan Dracula, yayımlanmasının üzerinden yüz yılı aşkın süre geçmesine karşın, bugün de aynı ilgiyle okunuyor.",
-        },
-        {
-          title: "Sultanın Korsanları",
-          author: "Emrah Safa Gürkan",
-          pageNum: 296,
-          imageURL:
-            "https://i.idefix.com/cache/600x600-0/originals/0001780787001-1.jpg",
-          topic:
-            "1968-1970 yılları arasında geçen olaylar, o günün toplumsal gerçeklerini de satırlara taşıyor. Ama romanın odağında bu toplumsal olaylar değil üçlü bir aşk var. Gençliğin rüzgârıyla hareketlenen İmkânsızın Şarkısını ölümle erken karşılaşan gençlerin hayatı yönlendiriyor. Hiçbir şeyin önem taşımadığı, amaçsızlığın ağır bastığı, özgür seksin kol gezdiği bir öğrenci hayatı... Ama diğer yanda da yoğun duygular var... İmkânsız aşklar, imkânsız şarkılar söyleten. Hemen hemen her Japon gencinin okuduğu roman anayurdu dışında da çok kişi tarafından sahipleniliyor.",
-        },
-      ]
-);
+export const BookContext = React.createContext();
 
+class BookContextProvider extends React.Component {
+  state = {
+    books: [
+      {
+        title: "Kırmızı Pazartesi",
+        author: "Gabriel Garcia Marquez",
+        pageNum: 296,
+        imageURL:
+          "https://i.idefix.com/cache/600x600-0/originals/0000000064101-1.jpg",
+        topic:
+          "1968-1970 yılları arasında geçen olaylar, o günün toplumsal gerçeklerini de satırlara taşıyor. Ama romanın odağında bu toplumsal olaylar değil üçlü bir aşk var. Gençliğin rüzgârıyla hareketlenen İmkânsızın Şarkısını ölümle erken karşılaşan gençlerin hayatı yönlendiriyor. Hiçbir şeyin önem taşımadığı, amaçsızlığın ağır bastığı, özgür seksin kol gezdiği bir öğrenci hayatı... Ama diğer yanda da yoğun duygular var... İmkânsız aşklar, imkânsız şarkılar söyleten. Hemen hemen her Japon gencinin okuduğu roman anayurdu dışında da çok kişi tarafından sahipleniliyor.",
+      },
+      {
+        title: "Şeker Portakalı",
+        author: "Jose Muro de Vasconselos",
+        pageNum: 200,
+        imageURL:
+          "https://i.idefix.com/cache/600x600-0/originals/0000000064031-1.jpg",
+        topic:
+          "İrlandalı yazar Bram Stoker’ın, iki taraf arasındaki bu irade ve güç çatışmasını işlediği ve korku edebiyatının başyapıtlarından biri sayılan Dracula, yayımlanmasının üzerinden yüz yılı aşkın süre geçmesine karşın, bugün de aynı ilgiyle okunuyor.",
+      },
+      {
+        title: "En Uzun Yüzyıl",
+        author: "İlber Ortaylı",
+        pageNum: 296,
+        imageURL:
+          "https://i.idefix.com/cache/600x600-0/originals/0001744876001-1.jpg",
+        topic:
+          "1968-1970 yılları arasında geçen olaylar, o günün toplumsal gerçeklerini de satırlara taşıyor. Ama romanın odağında bu toplumsal olaylar değil üçlü bir aşk var. Gençliğin rüzgârıyla hareketlenen İmkânsızın Şarkısını ölümle erken karşılaşan gençlerin hayatı yönlendiriyor. Hiçbir şeyin önem taşımadığı, amaçsızlığın ağır bastığı, özgür seksin kol gezdiği bir öğrenci hayatı... Ama diğer yanda da yoğun duygular var... İmkânsız aşklar, imkânsız şarkılar söyleten. Hemen hemen her Japon gencinin okuduğu roman anayurdu dışında da çok kişi tarafından sahipleniliyor.",
+      },
+      {
+        title: "Dracula",
+        author: "Bram Stoker",
+        pageNum: 200,
+        imageURL:
+          "https://i.idefix.com/cache/600x600-0/originals/0001828853001-1.jpg",
+        topic:
+          "İrlandalı yazar Bram Stoker’ın, iki taraf arasındaki bu irade ve güç çatışmasını işlediği ve korku edebiyatının başyapıtlarından biri sayılan Dracula, yayımlanmasının üzerinden yüz yılı aşkın süre geçmesine karşın, bugün de aynı ilgiyle okunuyor.",
+      },
+      {
+        title: "Karamazov Kardeşler",
+        author: "Fyodor Mihayloviç Dostoyevski",
+        pageNum: 200,
+        imageURL:
+          "https://i.idefix.com/cache/600x600-0/originals/0001803800001-1.jpg",
+        topic:
+          "İrlandalı yazar Bram Stoker’ın, iki taraf arasındaki bu irade ve güç çatışmasını işlediği ve korku edebiyatının başyapıtlarından biri sayılan Dracula, yayımlanmasının üzerinden yüz yılı aşkın süre geçmesine karşın, bugün de aynı ilgiyle okunuyor.",
+      },
+      {
+        title: "Sultanın Korsanları",
+        author: "Emrah Safa Gürkan",
+        pageNum: 296,
+        imageURL:
+          "https://i.idefix.com/cache/600x600-0/originals/0001780787001-1.jpg",
+        topic:
+          "1968-1970 yılları arasında geçen olaylar, o günün toplumsal gerçeklerini de satırlara taşıyor. Ama romanın odağında bu toplumsal olaylar değil üçlü bir aşk var. Gençliğin rüzgârıyla hareketlenen İmkânsızın Şarkısını ölümle erken karşılaşan gençlerin hayatı yönlendiriyor. Hiçbir şeyin önem taşımadığı, amaçsızlığın ağır bastığı, özgür seksin kol gezdiği bir öğrenci hayatı... Ama diğer yanda da yoğun duygular var... İmkânsız aşklar, imkânsız şarkılar söyleten. Hemen hemen her Japon gencinin okuduğu roman anayurdu dışında da çok kişi tarafından sahipleniliyor.",
+      },
+    ],
+  };
+
+  render() {
+    return(
+      <BookContext.Provider value={this.state}>
+        {this.props.children}
+      </BookContext.Provider>
+    )
+  }
+}
+
+//console.log(myContext);
+
+export default BookContextProvider;
 
 // 1) Default Value ---> this.context
-
-
-
-
-
-
-
+// - Context nesnesi oluşturuldu.
+// - Başlangı. değeri verip export edildi.
+// - İlgili component'e export edildi.
+// - context type yardımıyla context ilgili component'e atandı.
+// - this.context yardımıyla ilgili veriye ulaşıldı.
 
 
 
 // 2) Provider ---> Consumer
+// - Class component oluşturuldu ve veri state içine koyuldu.
+// - Contex'e ait provider ile state'te bulunan veri alındı.
+// - Parent child ilişkisi (this.props.children)
+// - Veri göndereceğimiz component'e context import edildi.
+// - Render içerisinde consumer ile provider'dan gelen veri yakalandı.
