2868cd613b91f9c66d27789e0b99c775f7848b36
elifcetinkaya <eliifctnk@gmail.com>
2021-05-31 17:58:23 +0300

    dynamic css

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -8,38 +8,50 @@ class BookList extends React.Component {
   render() {
     console.log(this.context);
 
-    return(
-    <ThemeContext.Consumer>
-      {(contextTheme) => (
-        <BookContext.Consumer>
-          {(contextBook) => {
-            const {books} = contextBook;
-            const { isDarkTheme, dark, light } = contextTheme;
-            const theme = isDarkTheme ? dark : light;
+    return (
+      <ThemeContext.Consumer>
+        {(contextTheme) => (
+          <BookContext.Consumer>
+            {(contextBook) => {
+              const { books } = contextBook;
+              const { changeTheme, isDarkTheme, dark, light } = contextTheme;
+              const theme = isDarkTheme ? dark : light;
 
-            return (
-              <section className="page-section" style={{background: theme.bg, color: theme.txt}} id="portfolio">
-                <div className="container">
-                  <div className="text-center">
-                    <h2 className="section-heading text-uppercase">
-                      BookFolio
-                    </h2>
-                    <h3 className="section-subheading text-muted">
-                      Lorem ipsum dolor sit amet consectetur.
-                    </h3>
+              return (
+                <section
+                  className="page-section"
+                  style={{ background: theme.bg, color: theme.txt }}
+                  id="portfolio"
+                >
+                  <div className="container">
+                    <div className="text-center">
+                      <h2 className="section-heading text-uppercase">
+                        BookFolio
+                      </h2>
+                      <h3 className="section-subheading text-muted">
+                        Lorem ipsum dolor sit amet consectetur.
+                      </h3>
+                      <button
+                        type="button"
+                        className="btn btn-sm btn-info"
+                        style={{ marginTop: "-70px" }}
+                        onClick={changeTheme}
+                      >
+                        Change Theme
+                      </button>
+                    </div>
+                    <div className="row">
+                      {books.map((book, i) => {
+                        return <Book book={book} key={i} />;
+                      })}
+                    </div>
                   </div>
-                  <div className="row">
-                    {books.map((book, i) => {
-                      return <Book book={book} key={i} />;
-                    })}
-                  </div>
-                </div>
-              </section>
-            );
-          }}
-        </BookContext.Consumer>
-      )}
-    </ThemeContext.Consumer>
+                </section>
+              );
+            }}
+          </BookContext.Consumer>
+        )}
+      </ThemeContext.Consumer>
     );
   }
 }
